c405f2153f35a8796c6d25af416969a01e1aee99
ValentineOO <varlomxg@gmail.com>
2023-12-30 03:13:58 +0100

    fixed bug and created controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,21 +1,20 @@
 const express = require("express");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post("/", (req, res) => {
-  res.json({ msg: "POST a single workout" });
-});
+router.post("/", createWorkout);
 
 // DElETE a single workout
 router.delete("/:id", (req, res) => {
