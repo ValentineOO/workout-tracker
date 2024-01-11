7cd07b12c9c88d0e46d1460d8dedd2e2cc08d47d
ValentineOO <varlomxg@gmail.com>
2024-01-11 12:54:43 +0100

    finished the workout tracker

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,7 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+// date fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
   const handleClick = async () => {
@@ -22,8 +25,12 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
