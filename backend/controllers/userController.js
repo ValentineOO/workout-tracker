6de40f5be42975e034ff69b3d1cc64bbef0700a7
ValentineOO <varlomxg@gmail.com>
2024-01-12 15:06:10 +0100

    logging users in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,20 +4,16 @@ const jwt = require("jsonwebtoken");
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
-// login user
-const loginUser = async (req, res) => {
-  res.json({ message: "login user" });
-};
 
-// signup user
-const signupUser = async (req, res) => {
+// login a user
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.signup(email, password);
+    const user = await User.login(email, password);
 
     // create a token
-    const token = createToken(user._id)
+    const token = createToken(user._id);
 
     res.status(200).json({ email, token });
   } catch (error) {
@@ -25,7 +21,20 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = {
-  loginUser,
-  signupUser,
+// signup a user
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
+
+module.exports = { signupUser, loginUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,7 +23,7 @@ userSchema.statics.signup = async function (email, password) {
     throw Error("All fields must be filled");
   }
   if (!validator.isEmail(email)) {
-    throw Error("Email is not valid");
+    throw Error("Invalid Email");
   }
   if (!validator.isStrongPassword(password)) {
     throw Error("Password not strong enough");
@@ -32,13 +32,33 @@ userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
 
   if (exists) {
-    throw Error("Email already exists");
+    throw Error("Email already in use");
   }
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
   return user;
 };
 
